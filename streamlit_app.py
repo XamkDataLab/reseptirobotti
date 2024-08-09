b4ad6fd100b4e2d9f8b3fb8ed387b0600e5c348c
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-08-09 13:08:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import requests
-import pandas as pd
 from scholar.lens_metadata import *
 from patents.lens_metadata import *
 from utils.llm import *
-import time
 from openai import OpenAI
 
 token = st.secrets["mytoken"]
